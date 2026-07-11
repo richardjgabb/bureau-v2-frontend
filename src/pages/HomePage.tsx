e46d0d436a3716024505b61cf4db4db33064aa43
richardgabb <richardjgabb@gmail.com>
2026-07-11 18:22:33 +0100

    updating logo

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,11 +1,8 @@
-import MainHeader from "../components/Atoms/MainHeader/MainHeader"
-
 const HomePage = () => {
 
     return (
         <section className="flex flex-col gap-4 items-center">
-                <MainHeader text="Bureau"/>
-                <img src="/Bureau.png" alt="Bureau logo" className="w-32 h-32" />
+                <img src="/Bureau.png" alt="Bureau logo" className="w-[70vw] py-8" />
         </section>)
 }
 
